9313e9b5eb46d2a4f52811d74eb1bacaa07edf0a
Mehak Nauman <119690712+justmexmehak@users.noreply.github.com>
2024-08-15 07:39:37 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,17 +1,8 @@
-// Create Web Server
-// Create a new express.js web server
-// Create a new route for GET requests to /comments
-// Inside the route, send back the value of the global comments variable
-// Start the server on port 3001
-// Use the correct route to send back the comments array
-// Use the correct method to start the server
+// Create web server
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
 
-// comments.js
-const express = require('express');
-const app = express();
-
-app.get('/comments', (req, res) => {
-  res.send(comments);
-});
-
-app.listen(3001);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
